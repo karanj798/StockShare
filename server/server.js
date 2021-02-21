7f8dcb43b41f780056c13eeb79c8492856b45dbb
Karan <karanj798@gmail.com>
2021-02-21 11:16:50 -0500

    Fix chunks.js error

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use('/api/stocks/info', proxy)
 app.use('/api/stocks/transactions', transactions)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'))
+    app.use(express.static('client/build'))
     app.get('/*', (req, res) => {
         res.sendFile(path.join(path.resolve(path.dirname('')), 'client', 'build', 'index.html'))
     })
