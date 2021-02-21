4b932ea58139fb26d43fc482fe3b70f38ff1c55e
Karan <karanj798@gmail.com>
2021-02-21 01:28:13 -0500

    Fix import error

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const users = require('./routes/api/users')
 const transactions = require('./routes/api/transactions')
